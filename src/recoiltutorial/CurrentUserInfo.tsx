20b548eb888bd171646d9854376196f8d6e6af84
andhl <andhlwhffu@gmail.com>
2024-01-09 21:16:45 +0900

    README update

--- src/recoiltutorial/CurrentUserInfo.tsx
+++ src/recoiltutorial/CurrentUserInfo.tsx
@@ -1,5 +1,5 @@
 import { useRecoilValue } from "recoil";
-import { currentUserNameQuery } from "./recoil/user";
+import { currentUserNameQuery } from "../recoil/user";
 
 export default function CurrentUserInfo() {
   const userName = useRecoilValue(currentUserNameQuery);
